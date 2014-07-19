6478dcc72d7235e73b9f5a1c93488ce2d78fbe64
Patrick Logan <patrickdlogan@gmail.com>
2014-07-19 14:51:48 -0700

    Figuring out the API

--- web/reactivate.dart
+++ web/reactivate.dart
@@ -9,7 +9,11 @@ void main() {
   });
   JsObject commentBoxDescription = new JsObject.jsify({
     'displayName': 'CommentBox',
-    'render': () => ReactDOM.callMethod('div', [divAttributes, 'Hello, world! I am a comment box.'])
+    'render': () => ReactDOM.callMethod('div', 
+        [divAttributes, 
+         ReactDOM.callMethod('h2', [{}, 'Comments']),
+         'These are comments, are they not?'
+         ])
   });
   var CommentBox = React.callMethod('createClass', [commentBoxDescription]);
   JsObject commentBoxInstance = new JsObject(CommentBox, [null]);
