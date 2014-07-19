13cf504ea1315e827d0817421b7dae4282820919
Patrick Logan <patrickdlogan@gmail.com>
2014-07-19 14:59:47 -0700

    Children components

--- web/reactivate.dart
+++ web/reactivate.dart
@@ -4,18 +4,35 @@ import 'dart:js';
 void main() {
   JsObject React = context['React'];
   JsObject ReactDOM = React['DOM'];
-  JsObject divAttributes = new JsObject.jsify({
+  JsObject listAttributes = new JsObject.jsify({
+    'className': 'commentList'
+  });
+  JsObject formAttributes = new JsObject.jsify({
+    'className': 'commentForm'
+  });
+  JsObject boxAttributes = new JsObject.jsify({
     'className': 'commentBox'
   });
+  JsObject commentListDescription = new JsObject.jsify({
+    'render': () => ReactDOM.callMethod('div', 
+        [listAttributes, 'Hello, world! I am a comment list.'])
+  });
+  JsObject commentFormDescription = new JsObject.jsify({
+    'render': () => ReactDOM.callMethod('div', 
+        [listAttributes, 'Hello, world! I am a comment form.'])
+  });
+  JsObject CommentList = React.callMethod('createClass', [commentListDescription]);;
+  JsObject CommentForm = React.callMethod('createClass', [commentFormDescription]);; 
   JsObject commentBoxDescription = new JsObject.jsify({
     'displayName': 'CommentBox',
     'render': () => ReactDOM.callMethod('div', 
-        [divAttributes, 
+        [boxAttributes, 
          ReactDOM.callMethod('h2', [{}, 'Comments']),
-         'These are comments, are they not?'
+         new JsObject(CommentList, [null]),
+         new JsObject(CommentForm, [null])
          ])
   });
-  var CommentBox = React.callMethod('createClass', [commentBoxDescription]);
+  JsObject CommentBox = React.callMethod('createClass', [commentBoxDescription]);
   JsObject commentBoxInstance = new JsObject(CommentBox, [null]);
   Element div = querySelector('#reactivate_here_id');
   React.callMethod('renderComponent', [commentBoxInstance, div]);
